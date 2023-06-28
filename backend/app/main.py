9b8013e594cbe4f6a4e9f2dd26b7b681644985da
inchoxd <rjb2066@gmail.com>
2023-06-28 16:20:34 +0900

    created endpoints about quiz

---
+++ backend/app/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, HTTPException, Request, Header, Form, Body
+from fastapi.responses import HTMLResponse
+from fastapi.encoders import jsonable_encoder
+from typing import Optional
+from pydantic import BaseModel
+from process import *
+import os
+import json
+
+
+app = FastAPI()
+proc = Proc()
+
+##################################################
+# fastapi_BaseModel
+##################################################
+#class Session(BaseModel):
+#    session_id:str
+#
+#
+##################################################
+@app.get('/quiz')
+async def gen_quiz(mode:int, session_id: str):
+    if not is_session:
+        # 400を返す
+        raise HTTPException(
+                status_code=400,
+                detail='invalid error'
+                )
+    else:
+        return quiz_data
+
+
+@app.post('/quiz/answer')
+async def gen_answer(session_id: str):
+    if not is_session:
+        # 400を返す
+        raise HTTPException(
+                status_code=400,
+                detail='invalid error'
+                )
+    else:
+        return ans_data
