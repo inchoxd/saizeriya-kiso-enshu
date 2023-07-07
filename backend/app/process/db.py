d96280cb7205bf3fbe399441aff53d4028c4d478
inchoxd <rjb2066@gmail.com>
2023-07-08 00:58:07 +0900

    added new columns

--- backend/app/process/db.py
+++ backend/app/process/db.py
@@ -53,6 +53,8 @@ class ResultQuizLogs(Base):
     corrects_num = Column(Integer)                              # 正解した数
     incorrects_num = Column(Integer)                            # 不正解の数
     accuracy = Column(Integer)                                  # 正答率
+    crr_points = Column(Integer)                                # 正解したポイントの合計
+    incrr_points = Columns(Integer)                             # 不正解したポイントの合計
     ttl_points = Column(Integer)                                # 獲得したポイントの合計点数
     created_at = Column(DateTime)                               # 作成日時
     updated_at = Column(DateTime)                               # 更新日時
